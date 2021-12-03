ac8ec157be6b3d124d98dbedb881841578318abb
Ivan Dario Avila Martinez <idamkiller@hotmail.com>
2021-12-03 17:04:16 -0500

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "dcd651202bb99d83ae5efd98dec7a941",
 "icons/Icon-maskable-192.png": "df104d053a1cbb297d0b0c1599d99dc5",
 "icons/Icon-maskable-512.png": "cff4719d7a0462cfdfc546866ffad80d",
-"index.html": "b3fd2d39a6c779ee03fdda0c747ffa2b",
-"/": "b3fd2d39a6c779ee03fdda0c747ffa2b",
-"main.dart.js": "72549206dc76733732fdb2542e9dc2ec",
+"index.html": "f67e067624315befd15f96c0e2150e8c",
+"/": "f67e067624315befd15f96c0e2150e8c",
+"main.dart.js": "5e52e0c30f54e6bd94fd4da4dbe3a351",
 "manifest.json": "256470f702f80e1c1d31a8a22192a45c",
 "version.json": "52a324499bf1d6733d7824881d356251"
 };
